bf3dfc7eb2465186621ba68f3a3de36d41403df8
andre <andredanielsson@live.se>
2013-11-07 19:33:50 +0100

    second

--- binaryconversion.erl
+++ binaryconversion.erl
@@ -1,10 +1,18 @@
 -module(binaryconversion).
--export([binaryconv1/1, binaryconv2/1, binaryconv2/2]).
+-export([binaryconv1/1, binaryconv2/1, binaryconv2/2, exp/2, highestexp/2]).
 
 binaryconv1(0) ->
     [];
 binaryconv1(N) ->
     binaryconv1(N div 2) ++ [N rem 2].
+
+%Base-case
+exp(X, 1) ->
+	X;
+
+% X gets multiplied by X, Y amount of times.
+exp(X, Y) ->
+	exp(X, Y-1) * X.
 
 binaryconv2(N) ->
     binaryconv2(N, 1).
@@ -12,5 +20,14 @@ binaryconv2(N) ->
 binaryconv2(N, M) ->
     [];
 
-binaryconv2(N, M) ->
-    binaryconv2(2#N 
+%binaryconv2(N, M) ->
+
+% N - exponent, M - number
+% Returns the lowest exponent so that 2^N > M
+highestexp(N, M) ->
+    X = exp(2,N),
+    if X =< M ->
+       highestexp(N + 1, M);
+    true ->
+       N-1
+    end.
